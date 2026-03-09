17b7919a2c50951f03cfca1032e807cf76d5f1f2
GOZO-DEISGN <info@gozo-design.com>
2026-03-09 10:07:12 +0100

    Deploy: 226786 build from aaa1160

--- assets/shield-alert-CJxKveNX.js
+++ assets/shield-alert-CJxKveNX.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-B5XUnpIH.js";/**
+import{c as e}from"./index-BRWeiKHe.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
